d2add742a69d9b0b5706f8ff8f439697ede0eec9
Douglas Duteil <douglasduteil@gmail.com>
2014-09-15 02:03:27 +0200

    TRIAL 0

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var path = require('path');
+
+var gulp = require('gulp');
+
+var deploy = require('dd-deploy');
+
+var pkg = require(path.resolve(process.cwd(), 'package.json'));
+
+
+
+gulp.task('publish', function (cb) {
+  return deploy({
+    dirSrc: 'dist',
+    branch: 'dist',
+    repo: 'https://bfbbb2a1c4555f4e223f786a5006aa5cc3939270@github.com/douglasduteil/remote-repo-access-trial',
+    push: false,
+    verbose: true
+  }, cb)
+});
